92fc61b06dc7132b17e41b1a9be14c3ee6507bef
Pal68 <vpalaguto@yandex.ru>
2024-10-03 23:59:15 +0500

    Update hw.py

--- hw.py
+++ hw.py
@@ -1,2 +1,7 @@
-print("Hello W!")
-#пппппав
+def getColorChanelMatrix(img,chanel):
+    if chanel in range(0,3):
+        result_matrix=img[:,:,chanel]
+    else:
+        result_matrix = 0.299 * img[:, :, 0] + 0.587 * img[:, :, 1] + 0.114 * img[:, :, 2]
+    return result_matrix
+#end getColorChanelMatrix
